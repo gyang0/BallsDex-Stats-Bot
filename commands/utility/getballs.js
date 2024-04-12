21a9e1072116f5d8ba3cf7099afa691af17b72e6
Gene Yang <geneyang702@gmail.com>
2024-04-11 19:39:02 -0700

    Some work on /getballs command

--- commands/utility/getballs.js
+++ commands/utility/getballs.js
@@ -12,6 +12,8 @@ module.exports = {
     // https://stackoverflow.com/questions/63322284/discord-js-get-an-array-of-all-messages-in-a-channel
     // Getting a list of all messages in the channel
     async execute(interaction) {
+        interaction.reply("this may take a while, please wait a few minutes...");
+
         const channel = interaction.client.channels.cache.get(process.env.CHANNEL_ID);
         let messages = [];
 
@@ -35,14 +37,17 @@ module.exports = {
 
         // Conditions for filtering messages
         // 1. Must be from BallsDex
-        // 2. Must have image attachment
+        // 3. Must have content "A wild countryball appeared!"
         const msgs = messages
             .filter(m => {
                 return m.author.id === process.env.BALLSDEX_USER_ID;
             })
             .filter(m => {
+                return m.content === "A wild countryball appeared!";
+            })
+            /*.filter(m => {
                 // Must be an image attachment
-                if(m.attachments.size > 0){
+                if(m.attachments.size == 1){
                     m.attachments.forEach(attachment => {
                         if(!attachment.contentType.includes("image/"))
                             return false;
@@ -50,7 +55,22 @@ module.exports = {
                 }
 
                 return true;
-            });
+            });*/
+
+        channel.send("There are " + msgs.length + " BallsDex spawns in this channel.");
+
+        //console.log(msgs.length);
+
+        // Todo: make this ping the user who sent the message
+        //channel.send("There are " + msgs.length + " BallsDex spawns in this channel");
+
+        /*// These messages are the ones that have an attachment
+        for(let i = 0; i < msgs.length; i++){
+            // BallsDex spawns have 1 image attachment
+            if(m)
+        }*/
+
+        //await interaction.reply("Data length: " + messages.length);
 
     }
 };
