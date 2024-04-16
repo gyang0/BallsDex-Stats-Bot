478290086e091b4e708ca72a6f9089a342725084
Gene Yang <geneyang702@gmail.com>
2024-04-16 11:05:18 -0700

    finish checks for verified capture spawns

--- commands/utility/getballs.js
+++ commands/utility/getballs.js
@@ -3,8 +3,75 @@ const PNG = require('pngjs').PNG;
 const pixelmatch = require('pixelmatch');
 const jimp = require("jimp");
 
+const fs = require("fs");
+
 // Environment variables
 require('dotenv').config();
+
+
+// List of country names
+// From https://ballsdex.miraheze.org/wiki/Rarity_List, 4/15/2024
+// Hudson Bay Company and Polish Underground State were removed (uncatchable)
+let countries = [
+    "British Empire", "Reichtangle", "Russian Empire", "Kalmar Union", "Roman Empire",
+    "Qin Dynasty", "German Empire", "Austria-Hungary", "Hunnic Empire", "Japanese Empire",
+    "Republic of China", "Soviet Union", "United States", "Vatican", "Russia",
+    "China", "India", "Ancient Greece", "Japan", "Korea",
+    "Napoleonic France", "Ottoman Empire", "South Korea", "France", "Spanish Empire",
+    "Achaemenid Empire", "Macedon", "United Kingdom ", "Pakistan", "Ancient Egypt",
+    "Brazil", "Byzantium", "Greenland", "Portuguese Empire", "Qing",
+    "British Raj", "Carthage", "Italy", "Kingdom of Italy", "Egypt",
+    "Russian Soviet Federative Socialist Republic", "Turkey", "Iran", "Kingdom of Greece", "African Union",
+    "Arab League", "Confederate States", "Gaul", "Germania", "Indonesia",
+    "Mayan Empire", "Yugoslavia", "Germany", "Australia", "Hong Kong",
+    "Israel", "Xiongnu", "Spain", "Antarctica", "Saudi Arabia",
+    "Franks", "League of Nations", "Monaco", "Union of South Africa", "Ukraine",
+    "Canada", "Poland", "Sweden", "Macau", "Scotland",
+    "South Africa", "Greece", "VietNam", "Thailand", "North Korea",
+    "England", "European Union", "Francoist Spain", "Manchukuo", "NATO",
+    "Republican Spain", "United Arab Republic", "United Nations", "Warsaw Pact", "Weimar Republic",
+    "Zhou", "Algeria", "Argentina", "Bangladesh", "Colombia",
+    "Czechia", "Iraq", "Malaysia", "Mexico", "Myanmar",
+    "Netherlands", "Nigeria", "Norway", "Peru", "Philippines",
+    "Portugal", "Prussia", "Romania", "Singapore", "Switzerland",
+    "Syria", "Tuvalu", "UAE", "Venezuela", "Ukrainian SSR",
+    "Ancient Athens", "Ancient Sparta", "Babylon", "Czechoslovakia", "Ethiopian Empire",
+    "French Indochina", "Nauru", "Numidia", "Quebec", "Siam",
+    "South Vietnam", "Taiwan", "Wales", "West Germany", "Cuba",
+    "Kingdom of Egypt", "Angola", "Austria", "Azerbaijan", "Bahamas",
+    "Belarus", "Belgium", "Bolivia", "Bulgaria", "Chile",
+    "Croatia", "Cyprus", "DR Congo", "Denmark", "Ecuador",
+    "Ethiopia", "Finland", "Hungary", "Jordan", "Kazakhstan",
+    "Kenya", "Kuwait", "Libya", "Morocco", "North Vietnam",
+    "Oman", "Qatar", "San Marino", "Serbia", "Slovakia",
+    "Sri Lanka", "Sudan", "Tunisia", "Turkmenistan", "Uzbekistan",
+    "Yemen", "Faroe Islands", "Trinidad and Tobago", "East Germany", "Free France",
+    "Jamaica", "Maldives", "Northern Ireland", "Polish Underground State", "Tibet",
+    "Vichy France", "Andorra", "Brunei", "Byelorussian Soviet Socialist Republic", "Micronesia",
+    "Tonga", "Barbados", "Marshall Islands", "Armenia", "Bahrain",
+    "Cambodia", "Chad", "Equatorial Guinea", "Free Congo State", "Georgia",
+    "Ghana", "Guatemala", "Guyana", "Hudson Bay Company", "Ireland",
+    "Kyrgyzstan", "Latvia", "Lithuania", "Mali", "Malta",
+    "Mongolia", "New Zealand", "Samoa", "Slovenia", "Togo",
+    "Uganda", "Uruguay", "Zambia", "Zimbabwe", "Malawi",
+    "Costa Rica", "Dominica", "Guinea-Bissau", "Sao Tome and Principe", "Tannu Tuva",
+    "Seychelles", "Afghanistan", "Albania", "Belize", "Bosnia",
+    "Botswana", "Cameroon", "Ceylon", "Congo", "Cote d'Ivoire",
+    "Dominican Republic", "Eritrea", "Estonia", "Eswatini", "Fiji",
+    "Free City of Danzig", "Gambia", "Haiti", "Honduras", "Khiva",
+    "Laos", "Lebanon", "Liechtenstein", "Moldova", "Mozambique",
+    "Nepal", "Nicaragua", "Niger", "Palestine", "Paraguay",
+    "Saint Kitts and Nevis", "Saint Lucia", "Somaliland", "South Sudan", "South Yemen",
+    "Tajikistan", "Tanzania", "Western Sahara", "Cape Verde", "Guinea",
+    "Grenada", "Palau", "St. Vincent and The Grenadines", "Solomon Islands", "Vanuatu",
+    "Antigua and Barbuda", "Benin", "Bhutan", "Burkina Faso", "Burundi",
+    "Central African Republic", "Comoros", "El Salvador", "Gabon", "Hejaz",
+    "Iceland", "Kiribati", "Kosovo", "Lesotho", "Liberia",
+    "Luxembourg", "Madagascar", "Mauritania", "Mauritius", "Montenegro",
+    "Namibia", "North Macedonia", "Panama", "Papua New Guinea", "Paris Commune",
+    "Rwanda", "Senegal", "Sierra Leone", "Somalia", "Suriname",
+    "Timor-Leste", "Djibouti"
+];
 
 
 // https://www.youtube.com/watch?v=FKbC83Te1Xw
@@ -64,70 +131,6 @@ const compareImages = async (url1, url2) => {
 }
 
 
-// List of country names
-// From https://ballsdex.miraheze.org/wiki/Rarity_List, 4/15/2024
-// Hudson Bay Company and Polish Underground State were removed (uncatchable)
-let countries = [
-    "British Empire", "Reichtangle", "Russian Empire", "Scandinavia", "Roman Empire",
-    "Qin Dynasty", "German Empire", "Austria-Hungary", "Hunnic Empire", "Japanese Empire",
-    "Republic of China", "Soviet Union", "United States", "Vatican", "Russia",
-    "China", "India", "Ancient Greece", "Japan", "Korea",
-    "Napoleonic France", "Ottoman Empire", "South Korea", "France", "Spanish Empire",
-    "Achaemenid Empire", "Macedon", "United Kingdom ", "Pakistan", "Ancient Egypt",
-    "Brazil", "Byzantium", "Greenland", "Portuguese Empire", "Qing",
-    "British Raj", "Carthage", "Italy", "Kingdom of Italy", "Egypt",
-    "Russian SFSR", "Turkey", "Iran", "Kingdom of Greece", "African Union",
-    "Arab League", "Confederate States / CSA", "Gaul", "Germania", "Indonesia",
-    "Mayan Empire", "Yugoslavia", "Germany", "Australia", "Hong Kong",
-    "Israel", "Xiongnu", "Spain", "Antarctica", "Saudi Arabia",
-    "Franks", "League of Nations", "Monaco", "Union of South Africa", "Ukraine",
-    "Canada", "Poland", "Sweden", "Macau", "Scotland",
-    "South Africa", "Greece", "VietNam", "Thailand", "North Korea",
-    "England", "European Union", "Francoist Spain", "Manchukuo", "NATO",
-    "Republican Spain", "United Arab Republic", "United Nations", "Warsaw Pact", "Weimar Republic",
-    "Zhou", "Algeria", "Argentina", "Bangladesh", "Colombia",
-    "Czechia", "Iraq", "Malaysia", "Mexico", "Myanmar",
-    "Netherlands", "Nigeria", "Norway", "Peru", "Philippines",
-    "Portugal", "Prussia", "Romania", "Singapore", "Switzerland",
-    "Syria", "Tuvalu", "UAE", "Venezuela", "Ukrainian SSR",
-    "Ancient Athens", "Ancient Sparta", "Babylon", "Czechoslovakia", "Ethiopian Empire",
-    "French Indochina", "Nauru", "Numidia", "Quebec", "Siam",
-    "South Vietnam", "Taiwan", "Wales", "West Germany", "Cuba",
-    "Kingdom of Egypt", "Angola", "Austria", "Azerbaijan", "Bahamas",
-    "Belarus", "Belgium", "Bolivia", "Bulgaria", "Chile",
-    "Croatia", "Cyprus", "Democratic Republic of the Congo / DR Congo", "Denmark", "Ecuador",
-    "Ethiopia", "Finland", "Hungary", "Jordan", "Kazakhstan",
-    "Kenya", "Kuwait", "Libya", "Morocco", "North Vietnam",
-    "Oman", "Qatar", "San Marino", "Serbia", "Slovakia",
-    "Sri Lanka", "Sudan", "Tunisia", "Turkmenistan", "Uzbekistan",
-    "Yemen", "Faroe Islands", "Trinidad and Tobago", "East Germany", "Free France",
-    "Jamaica", "Maldives", "Northern Ireland", "Tibet", "Vichy France",
-    "Andorra", "Brunei", "Byelorussian SSR", "Micronesia", "Tonga",
-    "Barbados", "Marshall Islands", "Armenia", "Bahrain", "Cambodia",
-    "Chad", "Equatorial Guinea", "Free Congo State", "Georgia", "Ghana",
-    "Guatemala", "Guyana", "Ireland", "Kyrgyzstan", "Latvia",
-    "Lithuania", "Mali", "Malta", "Mongolia", "New Zealand",
-    "Samoa", "Slovenia", "Togo", "Uganda", "Uruguay",
-    "Zambia", "Zimbabwe", "Malawi", "Costa Rica", "Dominica",
-    "Guinea-Bissau", "Sao Tome and Principe", "Tannu Tuva", "Seychelles", "Afghanistan",
-    "Albania", "Belize", "Bosnia", "Botswana", "Cameroon",
-    "Ceylon", "Congo", "Côte d'Ivoire/Ivory Coast", "Dominican Republic", "Eritrea",
-    "Estonia", "Eswatini", "Fiji", "Free City of Danzig", "Gambia",
-    "Haiti", "Honduras", "Khiva", "Laos", "Lebanon",
-    "Liechtenstein", "Moldova", "Mozambique", "Nepal", "Nicaragua",
-    "Niger", "Palestine", "Paraguay", "Saint Kitts and Nevis", "Saint Lucia",
-    "Somaliland", "South Sudan", "South Yemen", "Tajikistan", "Tanzania",
-    "Western Sahara", "Cape Verde", "Guinea", "Grenada", "Palau",
-    "Saint Vincent and The Grenadines", "Solomon Islands", "Vanuatu", "Antigua and Barbuda", "Benin",
-    "Bhutan", "Burkina Faso", "Burundi", "Central African Republic / CAR", "Comoros",
-    "El Salvador", "Gabon", "Hejaz", "Iceland", "Kiribati",
-    "Kosovo", "Lesotho", "Liberia", "Luxembourg", "Madagascar",
-    "Mauritania", "Mauritius", "Montenegro", "Namibia", "North Macedonia",
-    "Panama", "Papua New Guinea", "Paris Commune", "Rwanda", "Senegal",
-    "Sierra Leone", "Somalia", "Suriname", "Timor-Leste", "Djibouti"
-];
-
-
 // https://stackoverflow.com/questions/63322284/discord-js-get-an-array-of-all-messages-in-a-channel
 /**
  * Returns an array of spawn data
@@ -137,7 +140,7 @@ let countries = [
  * @param numCaught - Map of {country name, # oft imes caught}. Values changed within function. MUST BE PRESENT.
  * @return spawns - Array of [spawn message IDs, image URL]
  */
-async function getUnverifiedImages(channel, verifiedCaught, numCaught){
+async function getSpawns(channel, verifiedCaught, numCaught){
     // Array of numeric BallsDex spawn message IDs
     let spawns = [];
 
@@ -167,10 +170,10 @@ async function getUnverifiedImages(channel, verifiedCaught, numCaught){
                             // The spawn message directed to by the parent of the catch message is verified caught.
                             verifiedCaught.set(msg.reference.messageId, true);
 
-                            if(numCaught.has(country)) numCaught[country]++;
-                            else numCaught.set(country, 0);
+                            if(numCaught.has(country)) numCaught.set(country, numCaught.get(country) + 1);
+                            else numCaught.set(country, 1);
 
-                            console.log(country);
+                            //console.log(country + ": " + numCaught.get(country));
                         }
 
                         else if(msg.content === "A wild countryball appeared!"){
@@ -178,7 +181,7 @@ async function getUnverifiedImages(channel, verifiedCaught, numCaught){
                             let imgURL = msg.attachments.first().url;
                             spawns.push([msg.id, imgURL]);
 
-                            console.log(imgURL);
+                            //console.log(imgURL);
                         }
                     }
                 });
@@ -189,7 +192,7 @@ async function getUnverifiedImages(channel, verifiedCaught, numCaught){
     }
 
     // Return the spawn message IDs
-    return spawnMsgIDs;
+    return spawns;
 }
 
 
@@ -200,59 +203,73 @@ module.exports = {
 
     async execute(interaction) {
         const channel = interaction.client.channels.cache.get(process.env.CHANNEL_ID);
-        //interaction.reply("this may take a while, please wait a few minutes...");
+        interaction.reply("This may take a while, please wait a few minutes...");
         
         // Map of numeric BallsDex spawn message IDs that were verified captured
         // i.e. ones for which we could find catch messages
         // { key: ID of spawn, value: true if verified caught }
-        /*let verifiedCaught = new Map();
+        let verifiedCaught = new Map();
 
         // Map of {key: country name, value: number of times it was caught}
         let numCaught = new Map();
 
         // Array of [spawn message ID, spawn image URL]
-        let spawns = await getUnverifiedImages(channel, verifiedCaught, numCaught);
+        let spawns = await getSpawns(channel, verifiedCaught, numCaught);
 
         
         // Determine the countries of unverified spawns
-        for(let i = 0; i < spawns.length; i++){
-            // We only want unverified spawns
-            if(verifiedCaught.has(spawnID)) continue;
+        let unverified = [];
 
+        for(let i = 0; i < spawns.length; i++){
             let spawnID = spawns[i][0];
             let spawnImgURL = spawns[i][1];
 
-            console.log("Unverified: " + spawnID + "...");
+            // We only want unverified spawns
+            if(verifiedCaught.has(spawnID)){
+                continue;
+
+            } else {
+                unverified.push(spawnImgURL);
+                //console.log("Unverified: " + spawnID + "...");
+            }
         }
 
         // Organize data into readable format
-        let dataContents = "";
-        for(let [key, val] of numCaught){
-            dataContents += key + ": " + val + "\n";
+        let dataContents = "There were a total of " + spawns.length + " ball spawns in this channel.\n";
+        dataContents += "Of these, " + (spawns.length - unverified.length) + " balls were caught.\n";
+        dataContents += (unverified.length + " balls remained uncaught.\n\n");
+        
+        dataContents += ">>>>>>>>>> Unverified spawn images (NOT COUNTED BY BOT, FACTOR THESE INTO THE TOTAL)\n";
+        dataContents += "For your own sanity, use https://www.openallurls.com/ to open these links.\n";
+        for(let i = 0; i < unverified.length; i++){
+            dataContents += unverified[i] + "\n";
         }
 
-        console.log(dataContents);*/
+        dataContents += "\n";
+        dataContents += ">>>>>>>>>> Data for verified spawns:\n";
+        for(let i = 0; i < countries.length; i++){
+            countries[i] = countries[i].toLowerCase();
 
-        let TEST_1 = "https://cdn.discordapp.com/attachments/1215530757192417290/1228494972957757540/nt_PRglZMZtTkzqfFk.png?ex=662c4019&is=6619cb19&hm=85063d5cbdb7e935d104fc3e232585f712331f3e19815c964ac514565b1bd389&";
-        let TEST_2 = "https://cdn.discordapp.com/attachments/1215530757192417290/1221217784919494676/nt_OKuiAAMWaPwqciF.png?ex=662d7631&is=661b0131&hm=d375f5d57b52aa45b7122d4fe66fa1c659417cd0dce81fe829dd76a2b38709be&";
-        
-        let res = await compareImages(TEST_1, TEST_2);
-        console.log(res + "% compatible");
+            if(numCaught.has(countries[i])){
+                dataContents += (countries[i] + ": " + numCaught.get(countries[i]) + "\n");
+            } else {
+                dataContents += countries[i] + ": 0\n";
+            }
+        }
 
         // Write data to file
-        /*fs.appendFile('data.txt', dataContents, function(err){
+        fs.writeFile('data.txt', dataContents, function(err){
             if(err) throw err;
         });
-
-
-        // Data as file
         channel.send({
             files: ["data.txt"],
-            content: `Here's your data:`
-        });*/
+            content: `<@${interaction.member.user.id}> Your data:`
+        });
 
-        // Todo: make this ping the user who sent the message
-        //channel.send("There are " + msgs.length + " BallsDex spawns in this channel.");
-        //console.log("There are " + messages.length + " BallsDex spawns in this channel.");
+        //console.log(dataContents);
+
+        // TODO (for bugfixing)
+        // If the country name isn't found in any of the folder names, output an error. This will fix most surface level bugs.
+        // Start testing image comparisons and its speed before committing to adding everything.
     }
 };
